7ec42ae69a21754b2e351c559015817c1ed8d1fb
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2021-06-01 21:08:34 -0400

    Migrate the send_message hack into Messageable.send_split

--- kaztron/discord_patches.py
+++ kaztron/discord_patches.py
@@ -4,21 +4,28 @@ Patches for discord.py v1.x.
 These are all hacky as hell and I'm not proud of 'em.
 """
 
+from typing import Union, Sequence, List
+
 import functools
 from types import MethodType
 from enum import Enum
 
+import discord
 from discord.ext import commands
+
+from kaztron.utils.cogutils import Limits, strutils
+from kaztron.utils.embeds import EmbedSplitter
 
 
 class Patches(Enum):
     smart_quotes = 0
-    command_logging = 1
-    everyone_filter = 2
+    # command_logging = 1
+    # everyone_filter = 2
     mobile_embeds = 3
+    send_split = 4
 
 
-def apply_patches(client: commands.Bot, excl=tuple()):
+def apply_patches(client: commands.Bot, excl: Sequence[Patches] = tuple()):
     """
     Apply all patches. See the Patches enum.
 
@@ -30,6 +37,8 @@ def apply_patches(client: commands.Bot, excl=tuple()):
         patch_smart_quotes(client)
     if Patches.mobile_embeds not in excl:
         patch_mobile_embeds()
+    if Patches.send_split not in excl:
+        patch_messageable_send_split()
 
 
 def patch_smart_quotes(client: commands.Bot):
@@ -96,3 +105,82 @@ def patch_mobile_embeds():
         return await old_send(self, content, **kwargs)
     # noinspection PyArgumentList
     Messageable.send = new_send
+
+
+def patch_messageable_send_split():
+    from discord.abc import Messageable
+
+    async def send_split(self, contents=None, *,
+                         embed: Union[discord.Embed, EmbedSplitter] = None,
+                         file: discord.File = None, files: List[discord.File] = None,
+                         split_lines=False, **kwargs) -> Sequence[discord.Message]:
+        """
+        Send a message. This method wraps the :meth:`discord.Messageable.send()` method and adds
+        automatic message splitting if the contents are too long for one message.
+
+        No parsing of Markdown is done for message splitting; this behaviour may break intended
+        formatting. For messages which may contain formatting, it is suggested you parse and split
+        the message instead of relying on auto-splitting.
+
+        See also :meth:`kaztron.utils.split_chunks_on` and :meth:`kaztron.utils.natural_split` for
+        manual control of splitting behaviour. See also :meth:`kaztron.utils.split_code_chunks_on`
+        for splitting code blocks manually.
+
+        See also :cls:`kaztron.utils.embeds.EmbedSplitter` for similar functionality in splitting
+        embeds.
+
+        :param contents: The content of the message to send. If this is missing, then the ``embed``
+            parameter must be present.
+        :param embed: The rich embed for the content. Also accepts EmbedSplitter instances, for
+            automatic splitting - in this case, the EmbedSplitter will be finalized by this method.
+        :param split_lines: If true, split on lines instead of words. This should only be used when
+            the message contents is known to contain many line breaks; otherwise, the splitting may
+            fail.
+        """
+        # prepare text contents
+        if not contents:
+            content_chunks = (contents,)
+        else:
+            if not split_lines:  # word splitting
+                content_chunks = strutils.natural_split(contents, Limits.MESSAGE)
+            else:  # line splitting
+                content_chunks = strutils.split_chunks_on(contents, Limits.MESSAGE, split_char='\n')
+
+        # prepare embed
+        try:
+            embed_list = embed.finalize()
+        except AttributeError:
+            embed_list = (embed,)
+
+        #
+        # prepare the arguments for each message to send
+        #
+        # strategy: output all text chunks before starting to output embed chunks
+        # so the last text chunk will have the first embed chunk attached
+        # this is because non-split messages usually have the embed appear after the msg -
+        # should be fairly rare for both msg and embed to be split
+        msgs_kwargs = []
+
+        for content_chunk in content_chunks:
+            msg_kwargs = kwargs.copy()
+            msg_kwargs['contents'] = content_chunk
+            msgs_kwargs.append(msg_kwargs)
+
+            # only on first message
+            kwargs['reference'] = None
+
+        msgs_kwargs[-1]['embed'] = embed_list[0]  # last text chunk has first embed
+
+        for embed_chunk in embed_list[1:]:
+            msg_kwargs = kwargs.copy()
+            msg_kwargs['embed'] = embed_chunk
+            msgs_kwargs.append(msg_kwargs)
+
+        # last message only
+        msgs_kwargs[-1]['file'] = file
+        msgs_kwargs[-1]['files'] = files
+
+        # finally, send all the messages and collect the returned Message objects
+        return tuple(await self.send(**msg_kwargs) for msg_kwargs in msgs_kwargs)
+
+    Messageable.send_split = send_split
--- kaztron/kazcog.py
+++ kaztron/kazcog.py
@@ -9,11 +9,7 @@ from discord.ext import commands
 
 from kaztron import KazClient
 from kaztron.config import KaztronConfig, SectionView
-from kaztron.utils.discord import Limits
-from kaztron.utils.embeds import EmbedSplitter
 from kaztron.utils import logging as logutils
-
-from kaztron.utils import cogutils
 
 logger = logging.getLogger(__name__)
 
@@ -231,67 +227,3 @@ class KazCog(commands.Cog):
         :return: variable name -> value
         """
         return {}
-
-    async def send_message(self, destination, contents=None, *, tts=False,
-                           embed: Union[discord.Embed, EmbedSplitter] = None,
-                           auto_split=True, split='word') -> Sequence[discord.Message]:
-        """
-        Send a message. This method wraps the :meth:`discord.Client.send_message` method and adds
-        automatic message splitting if a message is too long for one line.
-
-        No parsing of Markdown is done for message splitting; this behaviour may break intended
-        formatting. For messages which may contain formatting, it is suggested you parse and split
-        the message instead of relying on auto-splitting.
-
-        See also :meth:`kaztron.utils.split_chunks_on` and :meth:`kaztron.utils.natural_split` for
-        manual control of splitting behaviour. See also :meth:`kaztron.utils.split_code_chunks_on`
-        for splitting Markdown code blocks manually.
-
-        See also :cls:`kaztron.utils.embeds.EmbedSplitter` for similar functionality in splitting
-        embeds.
-
-        :param destination: he location to send the message (Channel, PrivateChannel, User, etc.)
-        :param contents: The content of the message to send. If this is missing, then the ``embed``
-            parameter must be present.
-        :param tts: Indicates if the message should be sent using text-to-speech.
-        :param embed: The rich embed for the content. Also accepts EmbedSplitter instances, for
-            automatic splitting - in this case, the EmbedSplitter will be finalized by this method.
-        :param auto_split: Whether to auto-split messages that exceed the maximum message length.
-        :param split: What to split on: 'word' or 'line'. 'Line' should only be used for messages
-            known to contain many line breaks, as otherwise auto-splitting is likely to fail.
-        """
-        # prepare text contents
-        if not contents or not auto_split:
-            content_chunks = (contents,)
-        else:
-            if split == 'word':
-                content_chunks = cogutils.strutils.natural_split(contents, Limits.MESSAGE)
-            elif split == 'line':
-                content_chunks = cogutils.strutils.split_chunks_on(
-                    contents, Limits.MESSAGE, split_char='\n')
-            else:
-                raise ValueError('`split` argument must be \'word\' or \'line\'')
-
-        # prepare embed
-        try:
-            embed_list = embed.finalize()
-        except AttributeError:
-            embed_list = (embed,)
-
-        # strategy: output all text chunks before starting to output embed chunks
-        # so the last text chunk will have the first embed chunk attached
-        # this is because non-split messages usually have the embed appear after the msg -
-        # should be fairly rare for both msg and embed to be split
-        msg_list = []
-        for content_chunk in content_chunks[:-1]:
-            msg_list.append(await self.bot.send_message(destination, content_chunk, tts=tts))
-
-        msg_list.append(await self.bot.send_message(
-            destination, content_chunks[-1], tts=tts, embed=embed_list[0]
-        ))
-
-        for embed_chunk in embed_list[1:]:
-            msg_list.append(await self.bot.send_message(destination, tts=tts, embed=embed_chunk))
-        return tuple(msg_list)
-
-

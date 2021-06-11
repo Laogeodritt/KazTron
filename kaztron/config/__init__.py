2368ab4857113a1c37b9301e6081b297c67e5376
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2021-06-10 20:08:11 -0400

    config reorg - move to package

---
+++ kaztron/config/__init__.py
@@ -0,0 +1,11 @@
+from .config import KaztronConfig
+from .config import JsonFileStrategy, TomlReadOnlyStrategy
+
+from .config import SectionView
+
+from .config import get_runtime_config, get_kaztron_config
+
+from .config import ConfigError, ConfigKeyError, ConfigConverterError, ReadOnlyError, \
+    ConfigNameError
+
+from .config import ChannelConfig, RoleConfig

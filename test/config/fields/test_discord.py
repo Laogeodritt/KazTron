4a80f7974edfe89566b00c3680a7f5083a6dfbfe
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2021-12-26 20:58:21 -0500

    Fix changed client API in test/config/fields/test_discord

--- test/config/fields/test_discord.py
+++ test/config/fields/test_discord.py
@@ -6,24 +6,30 @@ from kaztron.config import DiscordDummy
 
 
 class Dummy:
-    def __init__(self, id_):
+    def __init__(self, name, id_):
         self.id: int = id_
+        self.name: str = name
 
 
 class MockClient:
     def __init__(self):
         pass
 
-    def validate_channel(self, id_):
-        return Dummy(id_)
+    def get_channel(self, id_):
+        return Dummy("channel", id_)
+
+    def get_all_channels(self):
+        channels = [Dummy("exists", 123456789012345678), Dummy("other", 876543210987654321)]
+        for channel in channels:
+            yield channel
 
 
-class NotFoundClient:
+class NotFoundClient(MockClient):
     def __init__(self):
         pass
 
-    def validate_channel(self, id_):
-        raise ValueError(id_)
+    def get_channel(self, id_):
+        return None
 
 
 class TestGuildChannelField:
@@ -39,12 +45,19 @@ class TestGuildChannelField:
         assert not f.lazy
         assert not f.must_exist
 
-    def test_convert(self):
+    def test_convert_id(self):
         f = GuildChannelField()
         f.client = MockClient()
         ch = f.convert(123456789012345678)
         assert isinstance(ch, Dummy)
         assert ch.id == 123456789012345678
+
+    def test_convert_name(self):
+        f = GuildChannelField()
+        f.client = MockClient()
+        ch = f.convert("exists")
+        assert isinstance(ch, Dummy)
+        assert ch.name == "exists"
 
     def test_convert_non_existent(self):
         f = GuildChannelField(must_exist=False)
@@ -52,14 +65,22 @@ class TestGuildChannelField:
         ch = f.convert(123456789012345678)
         assert ch.id == 123456789012345678
 
+    def test_convert_name_non_existent(self):
+        f = GuildChannelField(must_exist=False)
+        f.client = NotFoundClient()
+        ch = f.convert("not-exists")
+        assert ch.name == "not-exists"
+
     def test_convert_non_existent_must_exist(self):
         f = GuildChannelField(must_exist=True)
         f.client = NotFoundClient()
         with pytest.raises(ValueError):
-            ch = f.convert(123456789012345678)
+            f.convert(123456789012345678)
+        with pytest.raises(ValueError):
+            f.convert("not-exists")
 
     @pytest.mark.parametrize("must_exist", [True, False])
     def test_serialise(self, must_exist):
         f = GuildChannelField(must_exist=must_exist)  # must_exist shouldn't matter
         f.client = MockClient()
-        assert f.serialize(Dummy(123456789012345678)) == 123456789012345678
+        assert f.serialize(Dummy("exists", 123456789012345678)) == 123456789012345678
